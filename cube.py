d18164d4e987c116e62bd5f22459ded61f9cbb6d
Pavan Bhonde <pavanbhonde001@gmail.com>
2026-09-09 22:40:15 +0530

    Calculate cube of user input

---
+++ cube.py
@@ -0,0 +1,8 @@
+# Get input from the user
+num = float(input("Enter a number: "))
+
+# Calculate the cube using the exponent operator
+cube = num ** 3
+
+# Print the result
+print(f"The cube of {num} is {cube}")
